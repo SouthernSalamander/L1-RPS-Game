286765e4dc727f2e77e6cf249bc7d810b62b8a57
SouthernSalamander <trinderj4203@masseyhigh.school.nz>
2026-04-01 10:51:18 +1300

    Working comparison component

--- B_01_rps_game_v1.py
+++ B_01_rps_game_v1.py
@@ -1,4 +1,26 @@
-# functions go here
+import random
+
+# Compares user and comp choice and returns
+# result (win / lose / tie)
+def rps_compare(user, comp):
+
+    # if the user and computer choice is the same, it's a tie
+    if user == comp:
+        result = "tie"
+
+    # There are three ways to win
+    elif user == "paper" and comp == "rock":
+        result = "win"
+    elif user == "scissors" and comp == "paper":
+        result = "win"
+    elif user == "rock" and comp == "scissors":
+        result = "win"
+
+    # if it's not a win / tie, then it's a loss
+    else:
+        result = "lose"
+
+    return result
 
 # Check that users have entered a valid
 # option based on a list
@@ -106,20 +128,28 @@ while rounds_played < num_rounds:
         rounds_heading = f"\n*** Round {rounds_played + 1} of {num_rounds} ***"
 
     print(rounds_heading)
-    print()
+    # randomly choose from the rps list (excluding the exit code)
+    comp_choice = random.choice(rps_list[:-1])
+    print("Computer choice: ", comp_choice)
+
 
     # get user choice
-    user_choice = string_checker("Choose: ", rps_list)
-    print(f"You chose {user_choice}")
+    user_choice = string_checker("Choose R / P / S: ", rps_list)
+    print(f"User Choice: {user_choice}")
 
+    # If user choice is the exit code, break the loop
     if user_choice == "xxx":
         break
+
+    result = rps_compare(user_choice, comp_choice)
+    print(f"{user_choice} vs {comp_choice}, {result} ")
 
     rounds_played += 1
 
     # if users are in infinite mode, increase round number
     if mode == "infinite":
         num_rounds += 1
+
 
 # Game loop ends here
 
